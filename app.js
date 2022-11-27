a923227664e2d49f8fcc1aba79985a7eda8b3a9c
Valerii from earth <lvaldev.code@gmail.com>
2022-11-27 21:33:17 +0100

    remove old comments

--- app.js
+++ app.js
@@ -19,50 +19,10 @@ mongoose
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for request
-// simple log of request
-// app.use((req, res, next) => {
-//   console.log("--------------------------------------");
-//   console.log(`host: ${req.hostname}`);
-//   console.log(`path: ${req.path}`);
-//   console.log(`method: ${req.method}`);
-//   next();
-// });
-
 //  middleware & static files
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-// app.get("/add-blog", (req, res) => {
-//   const blog = new Blog({
-//     title: "new blog 4",
-//     snippet: "about my new blog 4",
-//     body: "more about my new blog 4",
-//   });
-//   blog
-//     .save()
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => console.log(err));
-// });
-
-// app.get("/all-blogs", (_req, res) => {
-//   Blog.find()
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => console.log(err));
-// });
-// app.get("/single-blog", (_req, res) => {
-//   Blog.findById("6376af8ec58c2eb2b0930985")
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => console.log(err));
-// });
 
 // routes
 app.use(blogRoutes);
